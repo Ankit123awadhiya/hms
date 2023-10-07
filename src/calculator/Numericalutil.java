7a486fca1f8040a1ee4e64beeac96684518dd0ad
Ankit Kumar <ankitawadhiya18@gmail.com>
2023-10-07 17:48:12 +0530

    initial project setup

---
+++ src/calculator/Numericalutil.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class Numericalutil {
+
+	public static int add(int a , int b) {
+		int sum = 0;
+		if (a > 0 && b > 0) {
+			sum = a + b;
+		}
+         return sum;
+	}
+}
